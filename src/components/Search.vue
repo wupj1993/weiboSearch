14924247d49a30c27fa6a35549b4626caa1a35e6
wupj <wpjlovehome@gmail.com>
2017-03-15 23:17:14 +0800

    搜索界面增加生日作为查询条件，增加弹窗组件和日期选择器

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,10 +3,11 @@
     <div id="search">
       <div style="margin-top: 15px;">
         <el-input placeholder="请输入内容" v-model="searchValue">
-          <el-select v-model="select" slot="prepend" placeholder="请选择">
+          <el-select v-model="select" @change="selectChange" slot="prepend" placeholder="请选择">
             <el-option label="姓名" value="address"></el-option>
             <el-option label="地址" value="city"></el-option>
             <el-option label="邮箱号" value="email"></el-option>
+            <el-option label="出生日期" value="birthday"></el-option>
           </el-select>
           <el-button slot="append" icon="search"></el-button>
         </el-input>
@@ -14,7 +15,7 @@
     </div>
     <div class="table">
       <el-table :data="tableData" @row-click="rowClick" border style="width: 100%">
-        <el-table-column prop="name"  label="姓名" width="120">
+        <el-table-column prop="name" label="姓名" width="120">
         </el-table-column>
         <el-table-column prop="gender" label="性别" width="120"
                          :filters="[{ text: '女', value: 'F' }, { text: '男', value: 'M' }]"
@@ -44,6 +45,16 @@
         </el-pagination>
       </div>
     </div>
+    <el-dialog title="请选择日期" v-model="dialogVisible" size="tiny">
+      <div class="block">
+        <el-date-picker size="large" align="center" @change="timePickerChange" type="date" v-model="selectTimePicker"
+                        placeholder="选择日期">
+        </el-date-picker>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -55,7 +66,9 @@
         searchValue: '',
         select: '',
         tableData: [],
-        currentPage: 2
+        currentPage: 2,
+        dialogVisible: false,
+        selectTimePicker: ''
       }
     },
     methods: {
@@ -77,6 +90,14 @@
       handleCurrentChange: function (val) {
         this.currentPage = val
         console.log(`当前页: ${val}`)
+      },
+      selectChange: function (val) {
+        if (val === 'birthday') {
+          this.dialogVisible = true
+        }
+      },
+      timePickerChange: function (val) {
+        this.searchValue = val
       }
     },
     mounted: function () {
@@ -98,6 +119,10 @@
     width: 70%;
     margin: 0 auto;
 
+  }
+
+  .el-date-editor.el-input {
+    width: 100% !important;
   }
 
   .table {
